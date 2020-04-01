8162007159360bd772303abd8c0d1374806a71ac
PhamVietAnh99 <40080810+PhamVietAnh99@users.noreply.github.com>
2020-04-01 21:46:52 +0700

    bài tập

--- vietanh/resources/views/masterlayout.blade.php
+++ vietanh/resources/views/masterlayout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Bài tập</title>
 </head>
 <body>
     <H1>Header</H1>
